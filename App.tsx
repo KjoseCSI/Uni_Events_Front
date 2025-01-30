affc94038cc9fdb03a8ee3b88389e023874c2450
KjoseCSI <joserubenchicaiza@gmail.com>
2025-01-30 13:13:23 -0500

    Fixed merge

--- App.tsx
+++ App.tsx
@@ -1,40 +1,4 @@
-<<<<<<< HEAD
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import  MainEvents from './screens/Events/MainEvents'
-import { LinearGradient } from 'expo-linear-gradient';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import MapComponentView from './screens/Map/MapComponentView';
-import MapScreen from './screens/Map/MapScreen';
-import Navigation from "./navigation/Navigation";
-
-export default function App() {
-  return (
-
-      <Navigation/>
-
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    /* backgroundColor: '#004771' */
-  },
-  background: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    height: 1000,
-  },
-});
-=======
-import React from 'react';
-import { StyleSheet, View,Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginPage from './screens/LoginPage'; 
 import RegistrationPage from './screens/RegistrationPage';
 
 const Stack = createStackNavigator(); 
@@ -47,4 +11,4 @@ export default function App() {
 }
 
 
->>>>>>> Develop
+
