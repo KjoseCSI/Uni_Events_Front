257d8d95d0fa5a71dc5af9240ee184cc9c2f5f38
KjoseCSI <joserubenchicaiza@gmail.com>
2025-01-30 15:26:22 -0500

    Update Navegation
    Navigation Login and Registration

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import RegistrationPage from './screens/RegistrationPage';
+import Navigation from './navigation/Navigation';
 
 const Stack = createStackNavigator(); 
 export default function App() {
   return (
-    
-    <RegistrationPage/>
-  
+    <Navigation/>
   );  
 }
 
--- navigation/Navigation.tsx
+++ navigation/Navigation.tsx
@@ -1,18 +1,15 @@
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
 import {NavigationContainer} from "@react-navigation/native"
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { LinearGradient } from 'expo-linear-gradient';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
 
 //screens
 import MainEvents from "../screens/Events/MainEvents";
 import MapScreen from "../screens/Map/MapScreen";
 import React from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import LoginPage from '../screens/LoginPage'
+import RegistrationPage from "../screens/RegistrationPage"
+
 
 const Tab = createBottomTabNavigator();
 
@@ -44,25 +41,27 @@ function MyTabs() {
             options={{
                 tabBarIcon: ({focused ,color, size}) =>(
                     <MaterialIcons name="event" size={24} color="white"
-                    />
+                    /> 
                 )
             }}/>
             <Tab.Screen 
-            name="Mapas"
-            component={MapScreen}
+            name="Nuevo Usuario"
+            component={LoginPage}
             options={{
                 tabBarIcon: ({focused ,color, size}) =>(
-                    <MaterialCommunityIcons name="google-maps" size={24} color="white"
-                    />
+                    <AntDesign name="adduser" size={24} color="black" />
+                    /* <MaterialCommunityIcons name="google-maps" size={24} color="white"
+                    /> */
                 )
             }}/>
             <Tab.Screen 
             name="Perfil"
-            component={MapScreen}
+            component={RegistrationPage}
             options={{
                 tabBarIcon: ({focused ,color, size}) =>(
-                    <AntDesign name="team" size={24} color="white"
-                    />
+                    <AntDesign name="login" size={24} color="black" />
+                    /* <AntDesign name="team" size={24} color="white"
+                    /> */
                 )
             }}/>
         </Tab.Navigator>
