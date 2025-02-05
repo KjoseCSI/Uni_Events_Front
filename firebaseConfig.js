ff56a4b93b17d60081b93da44da062880ba7b809
KjoseCSI <joserubenchicaiza@gmail.com>
2025-02-05 15:45:29 -0500

    Fixed bug Firebase conection

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -16,7 +16,7 @@ import { getFirestore, collection } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: API_KEY ,
+    apiKey: API_KEY,
     authDomain: AUTH_DOMAIN,
     projectId: PROJECT_ID,
     storageBucket: STORAGE_BUCKET,
