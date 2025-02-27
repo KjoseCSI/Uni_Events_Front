0196a734a6fe8a25f014506508bbd7529864d4fc
Marina Andrango <120153905+Marina24AV@users.noreply.github.com>
2025-02-26 19:38:30 -0500

    Update UserProfile.tsx

--- screens/Events/UserProfile.tsx
+++ screens/Events/UserProfile.tsx
@@ -1,9 +1,8 @@
 
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Alert, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import * as ImagePicker from 'expo-image-picker';
-import { Alert } from 'react-native';
 import { useAuthContext } from '../../context/AuthContext';
 
 export default function UserProfile() {
